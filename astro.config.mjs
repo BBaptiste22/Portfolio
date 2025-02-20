c3bc5b9d51905984cd61d8f06564410f4ed51f83
baptiste.bernard <baptiste.bernard@lycee-elie-vinet.org>
2025-02-20 23:27:07 +0100

    ab

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+
+// If you are not going to use vercel analytics you can delete the analytics
+// If you want to remove vercel from your project you can do npm uninstall vercel
+export default defineConfig({
+  site: 'https://BBernard22.github.io',
+  base: 'Portfolio',
+  trailingSlash: 'always',
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
+
+//If you don't have vercel the configuration will be empty
+
+//ej : export default defineConfig({});
+//test
