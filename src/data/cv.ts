69c05e427f9d398c99564f20e30f04f20496b7b8
BBaptiste22 <bernard.baptiste08@gmail.com>
2025-03-26 09:50:43 +0100

    v2603

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -48,10 +48,23 @@ export const skills = [
 export const publications = [
 	{
 	  title: "GRAD",
-	  image: "/images/GRAD.png",
-	  description: "Site vitrine d'une entreprise d'installation de terasse, c'est un site fait en HTML, CSS et JS ainsi que bootstrap dans le cadre d'un devoir de première année de BTS.",
+	  image: "images/GRAD.png",
+	  description: "Site vitrine d'une entreprise d'installation de terasse, c'est un site fait en HTML, CSS, JS ainsi que bootstrap dans le cadre d'un devoir de première année de BTS.",
 	  link: "https://github.com/BBaptiste22/GRAD",
 	},
+	{
+		title: "Lioran",
+		image: "images/Lioran.png",
+		description: "Lioran est une application Android développée en Java pour la gestion d’une station de ski, avec une API Symfony pour la gestion des données. Ce projet a été réalisé dans le cadre de ma deuxième année de BTS SIO.",
+		link: "https://github.com/BBaptiste22/GRAD",
+	  },
+	  {
+		title: "G-Bien Formé",
+		image: "images/GBF.png",
+		description: "G-Bien Formé est une application web développée en Symfony avec une base de données PostgreSQL, elle facilite l’inscription des salariés à plusieurs formations. Ce projet a été réalisé dans le cadre de ma deuxième année de BTS SIO. ",
+		link: "https://github.com/BBaptiste22/GRAD",
+	  },
 	
-  ];
+
+];
   
