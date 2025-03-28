411bf3e235d3ed39465b8aaf574ce303eec8ebd7
baptiste.bernard <baptiste.bernard@lycee-elie-vinet.org>
2025-03-28 20:10:52 +0100

    v2.2803

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -48,31 +48,31 @@ export const skills = [
 export const publications = [
 	{
 	  title: "GRAD",
-	  image: "images/GRAD.png",
+	  image: `${import.meta.env.BASE_URL}images/GRAD.png`,
 	  description: "Site vitrine d'une entreprise de création de terasse, ce site est fait en HTML, CSS, JS ainsi que bootstrap dans le cadre d'un devoir de première année de BTS.",
 	  link: "https://github.com/BBaptiste22/GRAD",
 	},
 	{
 		title: "Lioran",
-		image: "images/lioran.png",
+		image: `${import.meta.env.BASE_URL}images/lioran.png`,
 		description: "Lioran est une application Android développée en Java pour la gestion d’une station de ski, avec une API Symfony pour la gestion des données. Ce projet a été réalisé dans le cadre de ma deuxième année de BTS SIO.",
 		link: "https://github.com/BBaptiste22/GRAD",
 	  },
 	  {
 		title: "G-Bien Formé",
-		image: "images/formation.png",
+		image: `${import.meta.env.BASE_URL}images/formation.png`,
 		description: "G-Bien Formé est une application web développée en Symfony avec une base de données PostgreSQL, elle facilite l’inscription des salariés à plusieurs formations. Ce projet a été réalisé dans le cadre de ma deuxième année de BTS SIO. ",
 		link: "https://github.com/BBaptiste22/GRAD",
 	  },
 	  {
 		title: "GAIA",
-		image: "images/GAIA.png",
+		image: `${import.meta.env.BASE_URL}images/GAIA.png`,
 		description: "Réalisation du site internet GAIA, dédié à la présentation du hackathon du Salon de l'Agriculture 2025, dans le cadre de mon stage de deuxième année au sein de l'entreprise Ekylibre. Ce site a été entièrement développé en HTML, CSS, JavaScript et Markdown.",
 		link: "https://github.com/BBaptiste22/GRAD",
 	  },
 	  {
 		title: "Tournois de pétanque",
-		image: "images/GAIA.png",
+		image: `${import.meta.env.BASE_URL}images/petanque.png`,
 		description: "",
 		link: "https://github.com/BBaptiste22/GRAD",
 	  },
