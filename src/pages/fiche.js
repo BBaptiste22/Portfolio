45e164bca4f20e279d9520d05fc1c9dc7c536576
baptiste.bernard <baptiste.bernard@lycee-elie-vinet.org>
2025-05-06 19:38:59 +0200

    Fix line endings and re-add filesss

--- src/pages/fiche.js
+++
@@ -1,7 +0,0 @@
- <script>
-    document.getElementById('openPdfLink').addEventListener('click', function(event) {
-      event.preventDefault();
-      // Remplacez 'votre-fichier.pdf' par le chemin de votre fichier PDF
-      window.open('/path/to/votre-fichier.pdf', '_blank');
-    });
-  </script>
